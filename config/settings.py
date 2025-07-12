15c319fa8eba739f09777c462ea7bdd6ed2cbe20
gtrirf <iravshanov90@gmail.com>
2025-07-13 02:32:49 +0500

    settings updated

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -15,7 +16,7 @@ DEBUG = env('DEBUG').lower() == 'true'
 SERVER_HOST = env('CURRENT_PROTOCOL') + '://' + env(
     'CURRENT_HOST') + f"{':' + env('CURRENT_PORT') if len(env('CURRENT_PORT')) else ''}"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["islombek.alwaysdata.net", "127.0.0.1", "localhost"]
 
 
 # Application definition
@@ -133,12 +134,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = [
+    "https://islombek.alwaysdata.net"
     'http://localhost:8000',
     'http://127.0.0.1:8000',
 ]
